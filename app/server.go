600bf43006715cac7c0e0e4fb3afd93270b05112
ShingoYadomoto <shingo.yadomoto@gmail.com>
2023-03-26 12:09:16 +0900

    add serve func

--- app/server.go
+++ app/server.go
@@ -6,6 +6,10 @@ import (
 	"os"
 )
 
+func serve(conn net.Conn) {
+	panic("not implemented")
+}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -15,9 +19,13 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go serve(conn)
 	}
 }
